c23e355d4e11a54fbb7564cc12dd0701af41ae7c
Hitoshi Yoshida <github@peria.jp>
2018-03-28 15:08:44 +0900

    Style change: Use 4-space indent

--- gomoku.py
+++ gomoku.py
@@ -6,81 +6,86 @@ N = 15
 # input: str[N][N] field : state of the field.
 # output: int[2] : where to put a stone in this turn.
 def Think(field):
-  CENTER = (int(N / 2), int(N / 2))
+    CENTER = (int(N / 2), int(N / 2))
 
-  best_position = (0, 0)
-  for i in range(N):
-    for j in range(N):
-      if field[i][j] != '.':
-        continue
+    best_position = (0, 0)
+    for i in range(N):
+        for j in range(N):
+            if field[i][j] != '.':
+                continue
 
-      position = (i, j)
-      # Assume to put a stone on (i, j).
-      field[i][j] = 'O'
-      if DoHaveFiveStones(field, position):
-        DebugPrint('I have a winning choice at (%d, %d)' % (i, j))
-        return position
-      # Revert the assumption.
-      field[i][j] = '.'
-      if GetDistance(best_position, CENTER) > GetDistance(position, CENTER):
-        best_position = position
-  return best_position
+            position = (i, j)
+            # Assume to put a stone on (i, j).
+            field[i][j] = 'O'
+            if DoHaveFiveStones(field, position):
+                DebugPrint('I have a winning choice at (%d, %d)' % (i, j))
+                return position
+            # Revert the assumption.
+            field[i][j] = '.'
+            if GetDistance(best_position, CENTER) > GetDistance(position, CENTER):
+                best_position = position
+    return best_position
+
 
 # Returns true if you have a five-stones line from |position|. Returns false otherwise.
 def DoHaveFiveStones(field, position):
-  return (CountStonesOnLine(field, position, (1, 1)) >= 5 or
-          CountStonesOnLine(field, position, (1, 0)) >= 5 or
-          CountStonesOnLine(field, position, (1, -1)) >= 5 or
-          CountStonesOnLine(field, position, (0, 1)) >= 5)
+    return (CountStonesOnLine(field, position, (1, 1)) >= 5 or
+            CountStonesOnLine(field, position, (1, 0)) >= 5 or
+            CountStonesOnLine(field, position, (1, -1)) >= 5 or
+            CountStonesOnLine(field, position, (0, 1)) >= 5)
+
 
 # Returns the number of stones on the line segment on the direction of |diff| from |position|.
 def CountStonesOnLine(field, position, diff):
-  count = 0
+    count = 0
 
-  row = position[0]
-  col = position[1]
-  while True:
-    if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'O':
-      break
-    row += diff[0]
-    col += diff[1]
-    count += 1
+    row = position[0]
+    col = position[1]
+    while True:
+        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'O':
+            break
+        row += diff[0]
+        col += diff[1]
+        count += 1
 
-  row = position[0] - diff[0]
-  col = position[1] - diff[1]
-  while True:
-    if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'O':
-      break
-    row -= diff[0]
-    col -= diff[1]
-    count += 1
+    row = position[0] - diff[0]
+    col = position[1] - diff[1]
+    while True:
+        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'O':
+            break
+        row -= diff[0]
+        col -= diff[1]
+        count += 1
 
-  return count
+    return count
+
 
 # Returns the Manhattan distance from |a| to |b|.
 def GetDistance(a, b):
-  return abs(a[0] - b[0]) + abs(a[1] - b[1])
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
 
 # Outputs |msg| to stderr; This is actually a thin wrapper of print().
 def DebugPrint(msg):
-  import sys
-  print(msg, file=sys.stderr)
+    import sys
+    print(msg, file=sys.stderr)
+
 
 # =============================================================================
 # DO NOT EDIT FOLLOWING FUNCTIONS
 # =============================================================================
 
 def main():
-  field = Input()
-  position = Think(field)
-  Output(position)
+    field = Input()
+    position = Think(field)
+    Output(position)
 
 def Input():
-  field = [list(input()) for i in range(N)]
-  return field
+    field = [list(input()) for i in range(N)]
+    return field
 
 def Output(position):
-  print(position[0], position[1])
+    print(position[0], position[1])
 
-if __name__  == '__main__':
-  main()
+if __name__    == '__main__':
+    main()
--- server.py
+++ server.py
@@ -5,131 +5,130 @@ import sys
 import subprocess
 
 class Game(object):
-  N = 15
-  NUM_PLAYERS = 2
+    N = 15
+    NUM_PLAYERS = 2
 
-  def __init__(self):
-    self.board_ = [[0] * Game.N for _ in range(Game.N)]
-    self.playing_id_ = 1
-    self.passes_ = 0
+    def __init__(self):
+        self.board_ = [[0] * Game.N for _ in range(Game.N)]
+        self.playing_id_ = 1
+        self.passes_ = 0
 
-  def get_state(self, id):
-    return '\n'.join([''.join([self.get_stone(i, j, id) for j in range(Game.N)])
-                      for i in range(Game.N)]) + '\n'
+    def get_state(self, id):
+        return '\n'.join([''.join([self.get_stone(i, j, id) for j in range(Game.N)])
+                          for i in range(Game.N)]) + '\n'
 
-  def get_stone(self, i, j, c):
-    if i not in range(Game.N) or j not in range(Game.N):
-      return ' '
-    b = self.board_[i][j]
-    return '.' if b is 0 else 'O' if b == c else 'X'
+    def get_stone(self, i, j, c):
+        if i not in range(Game.N) or j not in range(Game.N):
+            return ' '
+        b = self.board_[i][j]
+        return '.' if b is 0 else 'O' if b == c else 'X'
 
-  def put_stone(self, place):
-    i, j = place[0], place[1]
-    if i < 0 or j < 0 or i >= Game.N or j >= Game.N or self.board_[i][j] != 0:
-      return False
-    self.board_[i][j] = self.playing_id_
-    self.next_turn()
-    self.passes_ = 0
-    return True
+    def put_stone(self, place):
+        i, j = place[0], place[1]
+        if i < 0 or j < 0 or i >= Game.N or j >= Game.N or self.board_[i][j] != 0:
+            return False
+        self.board_[i][j] = self.playing_id_
+        self.next_turn()
+        self.passes_ = 0
+        return True
 
-  def play_pass(self):
-    self.next_turn()
-    self.passes_ += 1
-    return self.passes_ < Game.NUM_PLAYERS
+    def play_pass(self):
+        self.next_turn()
+        self.passes_ += 1
+        return self.passes_ < Game.NUM_PLAYERS
 
-  def next_turn(self):
-    self.playing_id_ = self.playing_id_ % Game.NUM_PLAYERS + 1
+    def next_turn(self):
+        self.playing_id_ = self.playing_id_ % Game.NUM_PLAYERS + 1
 
-  def is_over(self):
-    for i in range(Game.N):
-      for j in range(Game.N):
-        c = self.board_[i][j]
-        if c == 0:
-          continue
-        if (all(self.get_stone(i+k, j, c) == 'O' for k in range(5)) or
-            all(self.get_stone(i, j+k, c) == 'O' for k in range(5)) or
-            all(self.get_stone(i+k, j+k, c) == 'O' for k in range(5)) or
-            all(self.get_stone(i+k, j-k, c) == 'O' for k in range(5))):
-          return True
-    return False
+    def is_over(self):
+        for i in range(Game.N):
+            for j in range(Game.N):
+                c = self.board_[i][j]
+                if c == 0:
+                    continue
+                if (all(self.get_stone(i+k, j, c) == 'O' for k in range(5)) or
+                    all(self.get_stone(i, j+k, c) == 'O' for k in range(5)) or
+                    all(self.get_stone(i+k, j+k, c) == 'O' for k in range(5)) or
+                    all(self.get_stone(i+k, j-k, c) == 'O' for k in range(5))):
+                    return True
+        return False
 
 
 class Player(object):
-  def __init__(self, id, filepath):
-    self.id_ = id
-    self.path_ = filepath
+    def __init__(self, id, filepath):
+        self.id_ = id
+        self.path_ = filepath
 
-  def play(self, game):
-    state = game.get_state(self.id)
-    child = subprocess.Popen(self.command,
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE,
-                             shell=True)
-    place = child.communicate(state.encode())[0]
-    return list(map(int, place.strip().split(b' ')))
+    def play(self, game):
+        state = game.get_state(self.id)
+        child = subprocess.Popen(self.command,
+                                 stdin=subprocess.PIPE,
+                                 stdout=subprocess.PIPE,
+                                 shell=True)
+        place = child.communicate(state.encode())[0]
+        return list(map(int, place.strip().split(b' ')))
 
-  @property
-  def id(self):
-    return self.id_
+    @property
+    def id(self):
+        return self.id_
 
-  @property
-  def path(self):
-    return self.path_
+    @property
+    def path(self):
+        return self.path_
 
-  @property
-  def command(self):
-    return 'python3 ' + self.path
+    @property
+    def command(self):
+        return 'python3 ' + self.path
 
 
 class Gomoku(object):
-  def __init__(self, ai):
-    self.players_ = [Player(i + 1, ai[i]) for i in [0, 1]]
-    self.game_ = Game()
+    def __init__(self, ai):
+        self.players_ = [Player(i + 1, ai[i]) for i in [0, 1]]
+        self.game_ = Game()
 
-  def run_game(self):
-    winner = self.run_loop()
-    if winner.id > 0:
-      print('{0} won the game'.format(winner.path))
-    else:
-      print('Draw game')
-
-  def run_loop(self):
-    game = self.game_
-    while True:
-      for player in self.players_:
-        print("({1}){0}'s turn: ".format(player.path, 'O' if player.id == 1 else 'X'),
-              end='')
-        place = player.play(game)
-        if game.put_stone(place):
-          # Dump board state from 1st player's view
-          print('Put ({0}, {1})'.format(place[0], place[1]))
-          print(game.get_state(1))
-          # Wins the game
-          if game.is_over():
-            return player
+    def run_game(self):
+        winner = self.run_loop()
+        if winner.id > 0:
+            print('{0} won the game'.format(winner.path))
         else:
-          print('Pass')
-          if not game.play_pass():
-            # draw game
-            return Player(0, '')
+            print('Draw game')
+
+    def run_loop(self):
+        game = self.game_
+        while True:
+            for player in self.players_:
+                print("({1}){0}'s turn: ".format(player.path, 'O' if player.id == 1 else 'X'), end='')
+                place = player.play(game)
+                if game.put_stone(place):
+                    # Dump board state from 1st player's view
+                    print('Put ({0}, {1})'.format(place[0], place[1]))
+                    print(game.get_state(1))
+                    # Wins the game
+                    if game.is_over():
+                        return player
+                else:
+                    print('Pass')
+                    if not game.play_pass():
+                        # draw game
+                        return Player(0, '')
 
 
 def main():
-  if len(sys.argv) == 1:
-    print("""
+    if len(sys.argv) == 1:
+        print("""
 Usage:
  {0} player1 player2
-    player1 and player2 are names of pythoncode, e.g. gomoku.py.
-    player1 works as the firsr-hand player, and player2 works as the second-hand player.
-    If you ommit player2, we assume 'gomoku.py' as player2.""".format(sys.argv[0]),
-          file=sys.stderr)
-    return
-  ai = [filename for filename in sys.argv[1:]]
-  if len(ai) < 2:
-    ai.append('gomoku.py')
-  gomoku = Gomoku(ai)
-  gomoku.run_game()
+        player1 and player2 are names of pythoncode, e.g. gomoku.py.
+        player1 works as the firsr-hand player, and player2 works as the second-hand player.
+        If you ommit player2, we assume 'gomoku.py' as player2.""".format(sys.argv[0]),
+                    file=sys.stderr)
+        return
+    ai = [filename for filename in sys.argv[1:]]
+    if len(ai) < 2:
+        ai.append('gomoku.py')
+    gomoku = Gomoku(ai)
+    gomoku.run_game()
 
 
 if __name__ == '__main__':
-  main()
+    main()
